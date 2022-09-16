26cd3f080cc40c85652902257dda532b34b9a73a
Ana clara <dev.anaclaramota@outlook.com>
2022-09-15 22:17:28 -0300

    new-commit

---
+++ ex-html-js/console.js
@@ -0,0 +1,8 @@
+function maior(n1,n2){
+    if(n1 < n2){
+        console.log('INVÁLIDO')
+    } else{
+        console.log ('VÁLIDO')
+    }
+}
+maior(6,5)
---
+++ ex-html-js/main.js
@@ -0,0 +1,11 @@
+    function maior( n1,n2){
+    let n1 = document.getElementById('n1')
+    let n2 = document.getElementById('n2')
+    let res = document.getElementById('res')
+    if(n1 < n2){
+        res.innerHTML='NUMERO INVÁLIDO'
+    } else{
+        res.innerHTML='NUMERO VÁLIDO'
+    }
+    
+}
