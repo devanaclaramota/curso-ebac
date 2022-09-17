11d00f383fdd203ad3e499385059c028951a2f17
Ana clara <dev.anaclaramota@outlook.com>
2022-09-17 12:57:25 -0300

    commit-ex-complete

--- ex-html-js/main.js
+++ ex-html-js/main.js
@@ -1,11 +1,24 @@
-    function maior( n1,n2){
-    let n1 = document.getElementById('n1')
-    let n2 = document.getElementById('n2')
-    let res = document.getElementById('res')
-    if(n1 < n2){
-        res.innerHTML='NUMERO INVÁLIDO'
-    } else{
-        res.innerHTML='NUMERO VÁLIDO'
-    }
-    
+
+function calcular(){
+
+    var tnum = document.getElementById('tnum')
+
+    var num1 = Number(tnum.value)
+
+    var tnum2 = document.getElementById('tnum2')
+
+    var num2 = Number(tnum2.value)
+
+    var resp  = document.getElementById('res')
+
+if (num1 > num2){
+
+    resp.innerHTML = `O valor é INVÁLIDO`
+
+} else{
+
+    resp.innerHTML = 'O valor é VÁLIDO'
+
+}
+ 
 }
