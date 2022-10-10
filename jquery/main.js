ac6d982f3c7714bde9b00c5a19b90171f7a4c842
Ana clara <dev.anaclaramota@outlook.com>
2022-10-10 17:46:47 -0300

    new-commit

--- jquery/main.js
+++ jquery/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   $('form').slideDown()
   })
 
+  //para selecionar o elemento a ser usado escreve-se $('form') por exemplo
+
+  
   $('#cancelar').click(function(){
     $('form').slideUp()
   })
@@ -11,9 +14,11 @@ $(document).ready(function(){
     e.preventDefault()
     const novaImagem = $('url').val()
     const novoItem = $('<li><li/>')
-    $(`$(<img src ="${novaImagem}"/>`).appendTo(novoItem)
+    $(`<img src ="${novaImagem}"/>`).appendTo(novoItem)
     
   $(novoItem).appendTo('ul')
   })
 
 })
+//O jquery funciona atraves do $ e dos () dentro do parentese informamos o seletor (document, h1, variavel)
+// .ready é quando o documento tiver pronto.
